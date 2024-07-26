8f5b9403441f1bd9842366d87718acd7ecd1096b
Adam Ribaudo <adam.ribaudo@velir.com>
2024-07-26 06:58:20 -0400

    rename sin data

--- R Scripts/generate data.R
+++ R Scripts/generate data.R
@@ -1,21 +1,21 @@
 library(tidyverse)
 
-df <- data.frame(
+df_sin <- data.frame(
   week_index = seq(1:52),
   sin_output = (sin(seq(from = 0,to = 3, length.out=52))) * 100,
   noise_a = sample(10:30, 52, replace = TRUE),
   noise_b = sample(10:30, 52, replace = TRUE)
 )
 
-df$output = df$sin_output + df$noise_a
-df$correlated_output <- df$sin_output * -1 + 100 + df$noise_b
+df_sin$output <- df_sin$sin_output + df_sin$noise_a
+df_sin$correlated_output <- df_sin$sin_output * -1 + 100 + df_sin$noise_b
 
-df <- df %>% select(week_index, output, correlated_output) %>% 
+df_sin <- df_sin %>% select(week_index, output, correlated_output) %>% 
   pivot_longer(cols = !week_index, names_to = "output_type", values_to = "metric")
 
-ggplot(data = df, aes(x = week_index, y = metric, color = output_type)) + 
+ggplot(data = df_sin, aes(x = week_index, y = metric, color = output_type)) + 
   geom_line() +
   theme_minimal()
 
-
+write.csv(df_sin,"sin.csv")
 
